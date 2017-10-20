b46e6659ec908e625233a3e8645b8f1a28501046
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-20 22:26:04 +0100

    Updated the links on the links page

--- Rakefile
+++ Rakefile
@@ -1,4 +1,4 @@
-# ENV['RACK_ENV'] ||= 'development'
+ENV['RACK_ENV'] ||= 'development'
 require './app/data_mapper_setup'
 
 namespace :db do
--- app/app.rb
+++ app/app.rb
@@ -52,7 +52,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    User.create(email: params[:email], password: params[:password])
+    user = User.create(email: params[:email], password: params[:password])
     session[:user_id] = user.id
     redirect '/links'
   end
